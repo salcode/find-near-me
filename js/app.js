be89c567bb8e0e1f09a9eed4b0f0ca8ab3c4b685
Sal Ferrarello <sal@ironcodestudio.com>
2018-07-06 08:59:54 -0400

    Add find nearest code

--- js/app.js
+++ js/app.js
@@ -26,23 +26,66 @@
 
 	} );
 
-	function coordsToText( coords ) {
-		// @see https://developer.mozilla.org/en-US/docs/Web/API/Coordinates.
-		return "Latitude: " + coords.latitude +
-			"\nLongitude: " + coords.longitude +
-			"\nAltitude: " + coords.altitude +
-			"\nAccuracy: " + coords.accuracy +
-			"\nAltitudeAccuracy: " + coords.altitudeAccuracy +
-			"\nHeading: " + coords.heading +
-			"\nSpeed: " + coords.speed;
+	function NearestFeature(latitude, longitude, features) {
+		var mindif = 99999;
+		var closest;
+
+		for (index = 0; index < features.length; ++index) {
+			var dif = LatLngDist(latitude, longitude, features[index].geometry.coordinates[1], features[index].geometry.coordinates[0]);
+			if (dif < mindif) {
+				closest = index;
+				mindif = dif;
+			}
+		}
+
+		props = features[closest].properties;
+
+		output.textContent =
+			props.FACILITY_NAME + " (" + props.FACILITY_OR_OFFICE_OR_AGENCY + ")\n"
+			+ props.ADDRESS_1 + "\n"
+			+ props.CITY + ", " + props.STATE + " " + props.ZIP + "\n"
+			+ props.PHONE + "\n\n"
+			+ "approximately " + Math.round( dif ) + " miles away";
 	}
 
 	/**
-	 * Display results of geolocation.
+	 * Use results of geolocation.
 	 */
 	function geolocationSuccess( position ) {
-		// Display results of geolocation.
-		output.textContent = coordsToText( position.coords );
+
+		// Load list of locations as xobj.
+		var xobj = new XMLHttpRequest();
+
+		// Update button text.
+		geolocateBtn.textContent='Using your location...';
+
+
+		// Specify MIME type other than the one provided by the server.
+		// @see https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/overrideMimeType
+		xobj.overrideMimeType("application/json");
+
+		xobj.open( 'GET', 'js/places_geo.geojson', true );
+
+		xobj.onreadystatechange = function () {
+			if ( xobj.readyState != 4 ) {
+				// The operation is NOT complete.
+				// @see https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+				return;
+			}
+			if ( xobj.status != "200" ) {
+				// Failure.
+				output.textContent = "Unable to load locations. Please try again later.";
+				return;
+			}
+			// Successfully loaded data.
+			var nearest = NearestFeature(
+				position.coords.latitude,
+				position.coords.longitude,
+				JSON.parse( xobj.responseText ).features
+			);
+    };
+
+		xobj.send(null);
 
 		// Hide Geolocate Button.
 		geolocateBtn.style.display = 'none';
